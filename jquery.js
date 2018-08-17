a1e437d0013e3ff20d0a4f8b994c9dd89a8da646
Jordan Parker <parkerj4321@gmail.com>
2018-08-16 21:47:04 -0500

    Homework with Step 2 completed

---
+++ jquery.js
@@ -0,0 +1,13 @@
+//==Step 2====
+
+$(document).ready(function () {
+    alert("Locked and Loaded");
+})
+
+//==Step 3==
+
+$('h1').css('border', "2px solid yellow");
+$('p#intro').css('border', "2px solid yellow");
+$('#page-content').css('border', "2px solid yellow");
+$('#gallery li').css('border', "2px solid yellow");
+
